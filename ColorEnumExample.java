d5b66bd33ac67145823252f3c0e121f9123c5153
Jaycon John B. Paro <139573056+JayconJohnParo@users.noreply.github.com>
2023-10-22 05:49:44 -0700

    Update ColorEnumExample.java

--- ColorEnumExample.java
+++ ColorEnumExample.java
@@ -1,11 +1,11 @@
-public class ColorEnumExample {
+public class ColorEnumerateExample {
     public enum Color {
         RED(255, 0, 0),
         GREEN(0, 255, 0),
         BLUE(0, 0, 255),
         YELLOW(255, 255, 0),
         CYAN(0, 255, 255),
-        MAGENTA(255, 0, 255);
+        PURPLE(255, 0, 255);
 
         private int red;
         private int green;
